75409e6f601d5baf6b56063f5ac463a90878010d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-07 14:09:36 +0600

    Add javascript practice task exercises

---
+++ milestones/milestone-03-hello-javascript/module-13-introduction-to-javascript/m11.03_task3.js
@@ -0,0 +1,22 @@
+/**
+ * Task-3
+You task is to divide the given number by 5 and show the remainder as the output.
+
+Input:
+The first line of the input contains the number.
+
+Output:
+Print the remainder.
+
+Sample Input:
+119
+
+Sample Output:
+4
+*/
+
+
+var num =  119;
+var remainder = num % 5;
+
+console.log(remainder)
