65915d8233446942167dd4c86fda2e90023556c7
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-07 13:43:08 +0600

    Add javascript variable examples (declare, assign, reassign, undefined)

---
+++ milestones/milestone-03-hello-javascript/module-13-introduction-to-javascript/m4.01_variable.js
@@ -0,0 +1,23 @@
+/**
+ * Variable ==> vary + able
+ * 5 things to declare a variable
+ */
+
+var weight = 38;
+var price = 75;
+var year = 1995;
+var money = 97;
+
+var age;    // undefined
+console.log(age);
+
+console.log(money);
+console.log(price);
+
+money = 500;
+console.log(money)
+
+
+var age = 17;
+age = 63;
+console.log(age)
