9fc700677165626b2fdf16b55dded7bbfaa8e778
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-07 14:08:17 +0600

    Add javascript mixed operators and type coercion practice

---
+++ milestones/milestone-03-hello-javascript/module-13-introduction-to-javascript/m10.02_mixed_op.js
@@ -0,0 +1,17 @@
+var first = "Mobarok"
+var second = "Tobarok"
+
+var fullName = first + second;
+console.log(fullName);
+
+console.log(40 + '2');  // 402
+console.log(40 - '2');  // 38
+console.log(40 * '2');  // 80
+
+
+/**
+ * Variable
+ * type --> typeof
+ * convert --> parseInt(), parseFloat()
+ * tofixed()
+*/
