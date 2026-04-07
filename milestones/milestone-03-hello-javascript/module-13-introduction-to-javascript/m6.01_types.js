ee8d8c5f4927305dc8802b17532c04d0d41d7510
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-07 13:49:59 +0600

    Add javascript data types examples (string, number, boolean, typeof)

---
+++ milestones/milestone-03-hello-javascript/module-13-introduction-to-javascript/m6.01_types.js
@@ -0,0 +1,2 @@
+var name = sadaruddin;
+console.log(name)
---
+++ milestones/milestone-03-hello-javascript/module-13-introduction-to-javascript/m6.02_types.js
@@ -0,0 +1,21 @@
+// String type
+
+var name = "sadar uddin";
+console.log(name)
+
+var address = "Rajshahi, guridor hi, i am robot.";
+var country = 'Bangladesh';
+
+var isRich = true;
+console.log(isRich)
+
+var passed = false;
+console.log(passed)
+
+console.log(typeof passed)
+console.log(typeof country)
+
+
+var price = 550;
+console.log(price)
+console.log(typeof price)
