510da0e48896ab4b99ecde6c290e63dc2353da62
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-07 14:00:43 +0600

    add javascript shorthand operator examples (+=, -=, *=)

---
+++ milestones/milestone-03-hello-javascript/module-13-introduction-to-javascript/m10.01_shorthand.js
@@ -0,0 +1,18 @@
+var price = 35;
+price = price + 10;
+console.log(price)
+
+
+var price = 35;
+price += 20;    // shorthand
+console.log(price)
+
+
+var a = 45;
+a -= 10;
+console.log(a);
+
+
+var orangePrice = 30;
+orangePrice *= 3;
+console.log(orangePrice)
