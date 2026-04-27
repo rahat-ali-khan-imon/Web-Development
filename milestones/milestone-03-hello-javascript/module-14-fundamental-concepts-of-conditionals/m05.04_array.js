0310e5f9bf378d20bafaa1759ad96148d46c5120
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 19:49:26 +0600

    Demonstrate array join method for converting array to string

---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m05.04_array.js
@@ -0,0 +1,8 @@
+// join() – array কে string বানানো
+
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits.join(","));
+console.log(fruits.join(" "));
+
+let fruit = ["Apple", "Banana", "Mango"];
+console.log(fruit.join("-"));
