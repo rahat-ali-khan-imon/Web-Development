40d3879f223e5844b2a81b906f0a88c4ae43cbb5
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 20:34:40 +0600

    Explain JavaScript variables, naming conventions, and basic operations

---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m01.01_variable.js
@@ -0,0 +1,35 @@
+/**
+ * Variable ==> container
+*/
+
+// number
+var sunglassPrice = 80;
+
+// string
+var subject = "CSE";
+
+//boolean
+var passed = true;
+
+
+/**
+ * Naming convention
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+*/
+
+var lastExamResult = 97;
+
+
+/**
+ * Operation
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNan
+ * 4. null
+ * 5. Number
+*/
