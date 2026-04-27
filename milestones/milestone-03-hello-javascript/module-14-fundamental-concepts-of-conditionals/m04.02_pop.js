f4992b1be0ecbf0cc8b63bd80a8c776d0dcde4a0
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 17:59:49 +0600

    Demonstrate array pop method and return values

---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m04.02_pop.js
@@ -0,0 +1,15 @@
+const friends =  ['Alice', 'Nila', 'Rafi', 'Kalik', 'Ali'];
+console.log(friends);
+
+friends.pop();
+console.log(friends);
+friends.pop();
+console.log(friends);
+
+const out3 = friends.pop();
+const out4 = friends.pop();
+console.log(out3, out4);
+console.log(friends);
+
+// push() → শেষে element যোগ করে
+// pop() → শেষের element remove করে
