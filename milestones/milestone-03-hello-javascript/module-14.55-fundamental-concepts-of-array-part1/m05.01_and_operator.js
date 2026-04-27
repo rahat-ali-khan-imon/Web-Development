276e36b6c72a25e4e9f568315660ec5100fbfc57
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 21:16:38 +0600

    Demonstrate logical AND (&&) operator in conditional statements

---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m05.01_and_operator.js
@@ -0,0 +1,8 @@
+let age = 25;
+let hasID = true;
+
+if(age >= 18 && hasID === true) {
+  console.log("Entry allowed");
+} else {
+  console.log("Entry denied");
+}
---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m05.02_and_operator.js
@@ -0,0 +1,8 @@
+let age = 16;
+let hasID = true;
+
+if (age >= 18 && hasID) {
+  console.log("Entry allowed");
+} else {
+  console.log("Entry denied");
+}
