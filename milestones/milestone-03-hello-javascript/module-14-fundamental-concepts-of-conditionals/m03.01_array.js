9e9ff5162edd2d3b023e42e53995c1836d142a89
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 16:52:54 +0600

    Add array index access and update examples

---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m03.01_array.js
@@ -0,0 +1,6 @@
+// Get Value by Index
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits[0]);     // Apple
+console.log(fruits[2]);    // Mango
---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m03.02_array.js
@@ -0,0 +1,6 @@
+// Set / Change Value by Index
+
+let fruits = ["Apple", "Banana", "Mango"];
+fruits[1] = "Orange";
+
+console.log(fruits);
