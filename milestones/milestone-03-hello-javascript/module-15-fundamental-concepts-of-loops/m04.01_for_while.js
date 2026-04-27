43909db5a9807a836acf2e11e7c279cfc0b45cef
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 00:07:28 +0600

    Compare for loop and while loop with same output

---
+++ milestones/milestone-03-hello-javascript/module-15-fundamental-concepts-of-loops/m04.01_for_while.js
@@ -0,0 +1,10 @@
+let i = 0;   // loop variable 
+while(i < 5) {  // loop condition
+    console.log(i); // loop statement
+    i++;    // loop variable change
+}
+console.log();
+
+for(let i = 0; i < 5; i++) {
+    console.log(i);
+}
