e5b0d4d796b0dc48577395e565ece5dca7e8625a
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 21:21:48 +0600

    Demonstrate logical OR (||) operator in conditional statements

---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m05.03_or_operator.js
@@ -0,0 +1,8 @@
+let isWeekend = true;
+let isHoliday = false;
+
+if (isWeekend || isHoliday) {
+  console.log("No office today");
+} else {
+    console.log("Hey, I am Robot.");
+}
---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m05.04_or_operator.js
@@ -0,0 +1,7 @@
+let marks = 35;
+
+if (marks >= 40 || marks === 35) {
+  console.log("You passed");
+} else {
+  console.log("Hey, hi, hello, I am robot.");
+}
