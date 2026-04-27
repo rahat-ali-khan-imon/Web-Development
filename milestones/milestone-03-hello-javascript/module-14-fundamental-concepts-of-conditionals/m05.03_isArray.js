3c57faa122c21033c494591633fe984676b94f7b
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 19:43:30 +0600

    Demonstrate safe array type checking using Array.isArray

--- milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m05.03_isArray.js
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m05.03_isArray.js
@@ -9,3 +9,16 @@ console.log(Array.isArray(friends));
 console.log(Array.isArray(nums));
 console.log(Array.isArray(food));
 console.log(Array.isArray(age));
+console.log();
+
+
+
+// Array কি না চেক করা — Array.isArray()
+
+let fruits = ["Apple", "Banana"];
+let name = "Rahim";
+
+console.log(Array.isArray(fruits)); // true
+console.log(Array.isArray(name));   // false
+
+// Array কি না safely check করার best way।
