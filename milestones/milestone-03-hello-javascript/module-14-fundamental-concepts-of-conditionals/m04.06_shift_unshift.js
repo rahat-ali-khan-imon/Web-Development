399e2f80a3ac3015f2b14a227212d19a53bbb1dd
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 19:16:47 +0600

    Add shift and unshift example

---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m04.06_shift_unshift.js
@@ -0,0 +1,11 @@
+const friends =  ['Alice', 'Nila', 'Rafi', 'Kalik', 'Ali'];
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+friends.unshift('Ghumailam');
+console.log(friends);
+
+// shift() → প্রথম element remove করে
+// unshift() → শুরুতে element যোগ করে
