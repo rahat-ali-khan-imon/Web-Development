53b7cfa1c1bc3630bc94d44312d98dacb1c82036
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 19:53:08 +0600

    Demonstrate array slice method for copying and extracting elements

---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m05.05_array.js
@@ -0,0 +1,12 @@
+// slice() – copy করা (original change হয় না)
+
+let numbers = [10, 20, 30, 40];
+let newArr = numbers.slice(1, 3);
+
+console.log(newArr);    // [20, 30]
+console.log(numbers);
+
+
+
+let num = [100, 200, 300, 40, 500];
+console.log(num.slice());
