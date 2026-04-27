cbf7e7a4dad8c5ba0b1385a163d844776a1ec110
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 22:09:23 +0600

    Demonstrate nested ternary operator for grading logic

---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m08.06_ternary.js
@@ -0,0 +1,10 @@
+// Nested Ternary (Advanced)
+
+let marks = 85;
+
+let grade = marks >= 80 ? "A" 
+           : marks >= 60 ? "B"
+           : marks >= 40 ? "C"
+           : "Fail";
+
+console.log(grade);
