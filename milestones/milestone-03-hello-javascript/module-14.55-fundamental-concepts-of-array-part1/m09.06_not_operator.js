7a572aaa7817ff5ee2a9211461808e4da2ca4816
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 22:25:18 +0600

    Demonstrate boolean conversion using double NOT (git add .) operator

---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m09.06_not_operator.js
@@ -0,0 +1,29 @@
+// Boolean Conversion Examples
+
+console.log(0);
+console.log(!0);
+console.log(!!0);        // false
+console.log();
+
+console.log(1);
+console.log(!1);
+console.log(!!1);        // true
+console.log();
+
+console.log(!"");
+console.log(!!"");       // false
+console.log();
+
+console.log("Hi");
+console.log(!"Hi");
+console.log(!!"Hi");     // true
+console.log();
+
+console.log(null);
+console.log(!null);
+console.log(!!null);     // false
+console.log();
+
+console.log(undefined);
+console.log(!undefined);
+console.log(!!undefined);// false
