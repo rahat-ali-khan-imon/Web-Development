db1d3a6eb7656488c13a83a52287c71ea74f3843
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 20:53:05 +0600

    Demonstrate basic if condition in JavaScript

---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m04.01_if.js
@@ -0,0 +1,25 @@
+if(3 < 10) {
+    console.log("I have a smaller value.")
+}
+console.log()
+
+
+if(13 < 10) {
+    console.log("I have a smaller value.")
+}
+console.log()
+
+
+var briyaniPrice = 500;
+
+if(briyaniPrice < 300) {
+    console.log("Mama give me some biryani.")
+}
+console.log()
+
+
+var briyaniPrice = 200;
+
+if(briyaniPrice < 300) {
+    console.log("Mama give me some biryani.")
+}
