405acdf9a5715de3d181241144f8001c1a0b3d98
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 16:13:10 +0600

    Add array declaration examples with multiple data types

---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m01.01_array.js
@@ -0,0 +1,9 @@
+const numbers = [12, 22, 54, 65];         // Array declaration
+const string = ["Alice", "Nila", "Ali", "Abul"];
+const bool = [true, false, true, false];
+const mixedArray = [12, "Alice", true, 15.34];
+
+console.log(numbers);
+console.log(string);
+console.log(bool);
+console.log(mixedArray);
