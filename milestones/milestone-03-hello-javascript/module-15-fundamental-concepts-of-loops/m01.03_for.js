f1db190ea738c47df4ce7e833477a1043abf45be
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 23:09:37 +0600

    Demonstrate for loop basics and repeated execution

---
+++ milestones/milestone-03-hello-javascript/module-15-fundamental-concepts-of-loops/m01.03_for.js
@@ -0,0 +1,20 @@
+// Without loop
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log();
+
+// With loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Hello");
+}
+
+/**
+ * for loop Syntax:
+for (initialization; condition; increment/decrement) {
+    // code block to execute
+}
+
+*/
