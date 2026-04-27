a4481b88872dd6ad0eaed6d3776a88606a2a480e
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 19:30:32 +0600

    Demonstrate array includes method with condition checking

---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m05.01_includes.js
@@ -0,0 +1,11 @@
+const friends =  ['Alice', 'Nila', 'Rafi', 'Kalik', 'Ali'];
+console.log(friends);
+
+console.log(friends.includes('Alice'));
+console.log(friends.includes('Khan'));
+
+if(friends.includes('Nila')) {
+    console.log("Party");
+} else {
+    console.log("No party");
+}
