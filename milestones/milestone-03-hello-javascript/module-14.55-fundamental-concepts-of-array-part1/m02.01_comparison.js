454e79bb002f49ade46736f1c33ccd6c26cc434e
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 20:49:26 +0600

    Demonstrate JavaScript comparison operators and equality checks

---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m02.01_comparison.js
@@ -0,0 +1,33 @@
+console.log("5 > 10:", 5 > 10);
+console.log("5 < 10:", 5 < 10);
+console.log("5 == 10:", 5 == 10);
+console.log("10 == 10:", 10 == 10);
+console.log("10 >= 10:", 10 >= 10);
+console.log("15 >= 10:", 15 >= 10);
+console.log("15 <= 10:", 15 <= 10);
+console.log("10 == 10.1:", 10 == 10.1);
+console.log()
+
+// -------------------------
+var bananaPrice = 40;
+var applePrice = 250;
+
+console.log(bananaPrice > applePrice);
+console.log("\n")
+
+/**
+ *  ==
+ * ===
+*/
+
+console.log("10 == 10:", 10 == 10)
+console.log("10 == '10':", 10 == '10')
+console.log()
+
+console.log("10 === '10':", 10 === '10')
+console.log("\n")
+
+
+// ----------------------
+console.log("10 != '10':", 10 != '10')
+console.log("10 !== '10':", 10 !== '10')
