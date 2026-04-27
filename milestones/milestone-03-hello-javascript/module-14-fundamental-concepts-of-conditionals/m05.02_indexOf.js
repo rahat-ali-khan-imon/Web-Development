6f222f02dce0ef90dd145519fb65075a45c823e6
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 19:44:04 +0600

    Demonstrate array indexOf method for element search

--- milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m05.02_indexOf.js
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m05.02_indexOf.js
@@ -3,3 +3,15 @@ console.log(friends);
 
 console.log(friends.indexOf('Kalik'));
 console.log(friends.indexOf('Hey'));
+console.log();
+
+
+
+// indexOf() – element এর index খোঁজা
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits.indexOf("Banana"));      // 1
+console.log(fruits.indexOf("Orange"));      // -1
+
+// না পেলে -1 return করে।
