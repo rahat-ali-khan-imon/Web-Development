bf3c9a44affe2b5442b85d085c4642058badb81b
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 22:22:49 +0600

    Demonstrate double NOT (clear) operator for boolean conversion

---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m09.05_not_operator.js
@@ -0,0 +1,12 @@
+// Double NOT (!!) – Advanced
+
+let value = "Hello";
+
+console.log(!value);
+console.log(!!value);
+console.log();
+
+// কারণ non-empty string হলো truthy value।
+
+console.log(!"")
+console.log(!" ")
