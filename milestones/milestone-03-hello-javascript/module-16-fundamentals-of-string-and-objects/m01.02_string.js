6a833c992791fa064c82552216399abeecdb72e0
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 02:17:08 +0600

    Demonstrate length and index access in array and string

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m01.02_string.js
@@ -0,0 +1,13 @@
+const numbers = [10, 20, 30, 40, 50];
+
+console.log(numbers.length);
+console.log(numbers[1]);
+console.log();
+
+
+const capital = 'Dha ka';
+
+console.log(capital);
+console.log(capital.length);
+console.log(capital[0]);
+console.log(capital[1]);
