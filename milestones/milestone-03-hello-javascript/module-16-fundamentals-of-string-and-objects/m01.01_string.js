bb87a9e0d6dbee290223ffb9827809f560d51c48
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 02:02:38 +0600

    Demonstrate different types of strings and typeof in JavaScript

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m01.01_string.js
@@ -0,0 +1,9 @@
+const country = 'Bangladesh';
+const division = "Newkhale";
+const district = `B-Baria`;
+const thana = new String('Demra');  // object
+
+console.log(country + ' --->> ' + typeof country);
+console.log(division + ' --->> ' + typeof division);
+console.log(district + ' --->> ' + typeof district);
+console.log(thana + '--->>' + typeof thana);  // object
