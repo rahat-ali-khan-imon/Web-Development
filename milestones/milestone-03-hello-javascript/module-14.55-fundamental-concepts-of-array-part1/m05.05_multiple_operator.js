5b95396463625c60a32a7349c8e9ecd01837c68e
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 21:26:04 +0600

    Demonstrate multiple logical conditions and real world examples

---
+++ milestones/milestone-03-hello-javascript/module-14.55-fundamental-concepts-of-array-part1/m05.05_multiple_operator.js
@@ -0,0 +1,9 @@
+let age = 22;
+let hasLicense = true;
+let hasHelmet = false;
+
+if ((age >= 18 && hasLicense) && hasHelmet) {
+  console.log("You can ride");
+} else {
+  console.log("Not allowed");
+}
