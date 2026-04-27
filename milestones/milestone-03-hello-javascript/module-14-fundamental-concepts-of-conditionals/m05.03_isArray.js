bd5b3f5152b45abe08d16a9ce2147741cadc86af
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 19:38:51 +0600

    Demonstrate Array.isArray method for type checking

---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m05.03_isArray.js
@@ -0,0 +1,11 @@
+const friends =  ['Alice', 'Nila', 'Rafi', 'Kalik', 'Ali'];
+console.log(friends);
+
+const nums = [];
+const food = 'ros o gol la';
+const age =  63;
+
+console.log(Array.isArray(friends));
+console.log(Array.isArray(nums));
+console.log(Array.isArray(food));
+console.log(Array.isArray(age));
