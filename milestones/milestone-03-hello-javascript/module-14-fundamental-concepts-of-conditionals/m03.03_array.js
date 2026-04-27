6134ab58101acd8ed074bebafd6fae468fd33811
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-27 17:04:00 +0600

    Add array length and last element access examples

---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m03.03_array.js
@@ -0,0 +1,5 @@
+// Array Length
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits.length);
---
+++ milestones/milestone-03-hello-javascript/module-14-fundamental-concepts-of-conditionals/m03.04_array.js
@@ -0,0 +1,5 @@
+// Last Element বের করার Smart Way
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits[fruits.length - 1]);
