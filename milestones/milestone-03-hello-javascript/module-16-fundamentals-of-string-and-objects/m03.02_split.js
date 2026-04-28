eae24eb1da49f59f0dd6dc536064b3406297082f
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 11:30:19 +0600

    Demonstrate split() method with different separators in JavaScript

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m03.02_split.js
@@ -0,0 +1,5 @@
+const sentence = 'I am a good and hardworking person.'     // string
+
+console.log(sentence.split());
+console.log(sentence.split(''));    // empty string
+console.log(sentence.split(' '));
---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m03.03_split.js
@@ -0,0 +1,10 @@
+const sentence = 'I am a good and hardworking person.'
+
+console.log(sentence.split());
+console.log(sentence.split('a'));
+
+
+const friendsStr = 'Rahim,Kahim,Dahim,Lahim,Fahim,Sahim';
+const friends = friendsStr.split(',');
+
+console.log(friends);
