2251df1f0414ef7519813f0c83d1cdad3517cde2
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 11:13:35 +0600

    Normalize strings using trim and toLowerCase for comparison

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m02.11_real_life.js
@@ -0,0 +1,8 @@
+// Smart Combo (Real Life Use)
+
+let email1 = "  TEST@GMAIL.COM ";
+let email2 = "test@gmail.com";
+
+if (email1.trim().toLowerCase() === email2.toLowerCase()) {
+    console.log("Email matched!");
+}
