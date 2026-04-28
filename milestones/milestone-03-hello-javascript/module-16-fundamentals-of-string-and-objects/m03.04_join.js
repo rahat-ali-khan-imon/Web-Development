8bfec073263633d87fb25bdb6e86694741aecd9d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 11:33:42 +0600

    Demonstrate split() method with different separators in JavaScript

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m03.04_join.js
@@ -0,0 +1,5 @@
+const realFriend = [ 'Rahim', 'Kahim', 'Dahim', 'Lahim', 'Fahim', 'Sahim' ];    // array
+
+console.log(realFriend.join());
+console.log(realFriend.join(' | '));
+console.log(realFriend.join('-'))
---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m03.05_join.js
@@ -0,0 +1,7 @@
+// join() (Array এর জন্য)
+// ⚠ মনে রাখবে → join() string এর না, array এর method।
+
+let words = ["I", "love", "JavaScript"];
+
+let sentence = words.join("-");
+console.log(sentence);
