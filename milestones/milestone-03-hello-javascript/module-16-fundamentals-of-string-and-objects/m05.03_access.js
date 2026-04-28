88110a4c8f21e6932f6171fb466ff8ce44b0ce39
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 21:36:36 +0600

    Demonstrate object property access using dot and bracket notation

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m05.03_access.js
@@ -0,0 +1,14 @@
+// Access Object Properties
+
+let person = {
+    name: "Rahim",
+    age: 25,
+    city: "Dhaka"
+};
+
+// Dot Notation (Most Common)
+console.log(person.name);
+console.log(person.age);
+
+// Bracket Notation
+console.log(person["city"]);    // যখন property name dynamic হয় তখন bracket use করি।
