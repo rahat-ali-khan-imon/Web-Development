e0ef7dd8ae397f903dc866809322b7e77816b3ad
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 22:57:22 +0600

    Demonstrate Object.values() method to get object values

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m08.02_values.js
@@ -0,0 +1,17 @@
+const computer = {
+    brand: 'lenovo',
+    price: 35000,
+    processor: 'intel',
+    hdd: '512gb',
+    monitor: 'hp'
+};
+
+const values = Object.values(computer);
+console.log(values);
+console.log(values[0]);
+
+/*
+ *2️⃣ Values
+👉 Object এর ভিতরের data গুলো হলো values।
+🔹 সব values বের করা
+*/
