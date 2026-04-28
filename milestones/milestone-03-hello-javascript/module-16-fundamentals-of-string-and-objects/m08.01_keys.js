9f5f163b23858059ed03ee6f2078a88a51db03d1
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 22:52:50 +0600

    Demonstrate Object.keys() method to get object property names

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m08.01_keys.js
@@ -0,0 +1,16 @@
+const computer = {
+    brand: 'lenovo',
+    price: 35000,
+    processor: 'intel',
+    hdd: '512gb',
+    monitor: 'hp'
+};
+
+const keys = Object.keys(computer);    // Object to Array
+console.log(keys);
+
+/*
+1️⃣ Keys (Property Names)
+👉 Object এর property নামগুলোকে keys বলে।
+🔹 সব keys বের করা
+*/
