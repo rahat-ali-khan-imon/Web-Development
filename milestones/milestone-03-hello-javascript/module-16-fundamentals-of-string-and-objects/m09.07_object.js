f230e7fa9a7830a8f692bae5dc54c7674ba5c63c
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 23:43:57 +0600

    Demonstrate object creation using new Object() method

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m09.07_object.js
@@ -0,0 +1,11 @@
+// 2️⃣ Using new Object()
+
+const student = new Object();
+
+student.name = "Rahim";
+student.age = 21;
+student.department = "CSE";
+
+console.log(student);
+
+// 🟡 এটা একটু পুরাতন style। Interview এ কম ব্যবহার হয়।
