f135112a009e517ee4a671d960286686a037c748
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 23:46:54 +0600

    Demonstrate object creation using class in JavaScript

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m09.08_modern_way.js
@@ -0,0 +1,15 @@
+// 4️⃣ Using Class (Modern Way) 🚀
+
+class Student {
+    constructor(name, age, department) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+}
+
+const s1 = new Student("Rahim", 21, "CSE");
+
+console.log(s1);
+
+// 🟢 Modern JavaScript এ recommended way।
