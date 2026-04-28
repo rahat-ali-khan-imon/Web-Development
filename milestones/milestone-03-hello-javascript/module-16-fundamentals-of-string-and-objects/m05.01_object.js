9c06a0c0c6ecb059deaede0359132d651f3acac8
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 21:15:55 +0600

    Demonstrate object creation and nested object structure in JavaScript

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m05.01_object.js
@@ -0,0 +1,22 @@
+// object declaration
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+};
+
+// Non primitive
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda mam',
+    examDate: '30 Feb',
+    chapters: ['First', 'Second', 'Third'],     // Array
+    exams: {                                   // Object
+        name: 'Final Exam',
+        marks: 100
+    }
+};
+
+console.log(bottle);
+console.log(subject);
---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m05.02_object.js
@@ -0,0 +1,9 @@
+// Basic Example
+
+let person = {
+    name: "Rahim",
+    age: 25,
+    city: "Dhaka"
+};
+
+console.log(person);
