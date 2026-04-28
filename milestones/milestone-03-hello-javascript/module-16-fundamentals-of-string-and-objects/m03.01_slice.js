0375e2b52e5984e40f815e78b5b723178d84ee30
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 11:27:02 +0600

    Demonstrate slice() method for extracting substrings

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m03.01_slice.js
@@ -0,0 +1,10 @@
+const address = 'andorkilla';
+const part = address.slice(2, 5);
+
+console.log(part);
+
+
+let text = 'JavaScript';
+console.log(text.slice(0, 4));
+console.log(text.slice(4));     // 4 থেকে শেষ পর্যন্ত।
+console.log(text.slice(2));
