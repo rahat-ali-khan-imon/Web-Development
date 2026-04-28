0f8de6f3b158d69359ba1f89e9a9ed409996675a
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 22:34:10 +0600

    Demonstrate object property access using dot and bracket notation

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m07.01_property.js
@@ -0,0 +1,13 @@
+const person = {
+    name: "Alice Ali",
+    age: 63,
+    profession: "Developer",
+    salary: 630000,
+    married: true
+};
+
+console.log(person);
+console.log(person.profession);  // dot notation
+
+const income = person.salary;
+console.log(income);
---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m07.02_property.js
@@ -0,0 +1,13 @@
+const person = {
+    name: "Alice Ali",
+    age: 63,
+    profession: "Developer",
+    salary: 630000,
+    married: true
+};
+
+// bracket notation
+console.log(person["age"]);
+
+const boyos = person['age'];
+console.log(boyos);
