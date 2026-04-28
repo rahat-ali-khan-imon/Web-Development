29243cb00f8fd76c8e9d077825d3ac19c6f9d908
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-28 22:32:37 +0600

    Demonstrate object property access and naming conventions in JavaScript

---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m07.03_property.js
@@ -0,0 +1,11 @@
+const person = {
+    name: "Alice Ali",
+    age: 63,
+    profession: "Developer",
+    salary: 630000,
+    married: true,
+    "fav places": ["Bandorban", "Saintmartin", "Kuakata"]
+}
+
+console.log(person["fav places"]);
+console.log(person["fav places"][0]);
---
+++ milestones/milestone-03-hello-javascript/module-16-fundamentals-of-string-and-objects/m07.04_property.js
@@ -0,0 +1,11 @@
+const person = {
+    name: "Alice Ali",
+    age: 63,
+    profession: "Developer",
+    salary: 630000,
+    married: true,
+    favPlaces: ["Bandorban", "Saintmartin", "Kuakata"] //Best Practice --> Space ছাড়া property name রাখলে ভালো হয় 
+}
+
+console.log(person.favPlaces);
+console.log(person.favPlaces[0]);
