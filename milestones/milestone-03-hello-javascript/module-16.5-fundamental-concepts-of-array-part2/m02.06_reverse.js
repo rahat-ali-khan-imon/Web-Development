6a77bd43e6974c3b854d389fcf82eb62b0ac4300
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-29 15:23:55 +0600

    Demonstrate basic array reverse using reverse() method

---
+++ milestones/milestone-03-hello-javascript/module-16.5-fundamental-concepts-of-array-part2/m02.06_reverse.js
@@ -0,0 +1,11 @@
+// 1️⃣ Basic Array Reverse (Most Common)
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.reverse();
+
+console.log(numbers);
+
+/*
+🧠 Important:
+👉 reverse() original array change করে (mutates array) ⚠
+*/
