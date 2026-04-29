ea2c762494d669111d10f32760d82406770dc957
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-29 15:12:30 +0600

    Demonstrate copying array using for...of loop

---
+++ milestones/milestone-03-hello-javascript/module-16.5-fundamental-concepts-of-array-part2/m02.03_array_copy.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const rev_numbers = [];
+
+for(const num of numbers) {
+    console.log(num);
+    rev_numbers.push(num);
+}
+
+console.log(rev_numbers);
