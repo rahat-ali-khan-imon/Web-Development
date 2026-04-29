ec433d8187e7b5f122cc53898a0d4f33e7d8fe1a
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-29 19:24:03 +0600

    Demonstrate basic function declaration and function calls

---
+++ milestones/milestone-03-hello-javascript/module-17-fundamentals-of-functions/m01.01_function.js
@@ -0,0 +1,26 @@
+const number = 45;
+
+// Function declare
+function fanOffKor() {
+    console.log("bosa theke uthe dara.");
+    console.log("walk towards the switch.");
+    console.log("click the switch to off of the fan.\n");
+}
+
+// Call the function
+fanOffKor();
+
+/* Function Structure
+function function_name(params) {
+    
+} 
+*/
+
+function brushTeeth() {
+    console.log("pick up to the brush.");
+    console.log("add paste.");
+    console.log("2 min goso and goso and fena felo.");
+}
+
+// Call the function
+brushTeeth();
