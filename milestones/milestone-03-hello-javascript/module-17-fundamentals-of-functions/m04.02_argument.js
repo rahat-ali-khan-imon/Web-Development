4ecc1d6ddf0502e2d5c2f09f70426f725fa8a88d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-29 20:28:40 +0600

    Demonstrate function for calculating difference using arguments

---
+++ milestones/milestone-03-hello-javascript/module-17-fundamentals-of-functions/m04.02_argument.js
@@ -0,0 +1,9 @@
+function difference(num1, num2) {
+    const diff = num1 - num2;
+    console.log(num1, num2, "difference is", diff);
+}
+
+const fatherAge = 40;
+const myAge = 23;
+
+difference(fatherAge, myAge);
