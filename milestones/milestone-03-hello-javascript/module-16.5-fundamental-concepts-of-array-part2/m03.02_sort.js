ac6fe89cb713a68eb8c9107bbae09b5fa84d4b4f
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-29 15:44:58 +0600

    Demonstrate array sort behavior with numbers using sort() method

---
+++ milestones/milestone-03-hello-javascript/module-16.5-fundamental-concepts-of-array-part2/m03.02_sort.js
@@ -0,0 +1,10 @@
+const numbers = [4, 7, 2, 8, 3, 6];
+
+/*
+Ascending sort --->> smaller to larger.
+Descender sort --->> larger to smaller.
+*/
+
+console.log(numbers);
+const numbers_acs = numbers.sort();
+console.log(numbers_acs);
