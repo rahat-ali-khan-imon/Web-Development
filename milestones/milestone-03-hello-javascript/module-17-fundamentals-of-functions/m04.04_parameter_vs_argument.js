4fa508fca6da60e80eb509575f716dd5e23cd6c2
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-29 20:33:05 +0600

    Demonstrate difference between parameters and arguments

---
+++ milestones/milestone-03-hello-javascript/module-17-fundamentals-of-functions/m04.04_parameter_vs_argument.js
@@ -0,0 +1,17 @@
+// Parameter vs Argument
+
+// ✅ Parameter --->> Function declaration এর ভিতরের variable
+function greet(name) { }
+// 👉 এখানে name হলো parameter
+
+
+// ✅ Argument --->> Function call করার সময় যে value পাঠাই
+greet("Rahim");
+// 👉 "Rahim" হলো argument
+
+
+/*
+Simple Formula:
+Parameter = Variable in function definition
+Argument = Actual value during function call
+*/
