b3444adc06b68c3a0332ab0fa0a7c6bf20146368
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-06-27 12:16:14 +0600

    Practice DOM traversal and element creation

---
+++ milestones/milestone-04-interactive-javascript-dom/module-22-tour-of-dom-document-object-model/m07.03_childNodes.js
@@ -0,0 +1,13 @@
+// Create element and set innerHTML or innerText
+const newChild = document.createElement('li');
+newChild.innerHTML = 'New born baby footballer';
+
+// Find the parent where you will and the child
+const playerList = document.getElementById('player-list');
+
+
+
+/*
+1. playerList
+<ul id=​"player-list">​…​</ul>​
+*/
---
+++ milestones/milestone-04-interactive-javascript-dom/module-22-tour-of-dom-document-object-model/m07.04_childNodes.js
@@ -0,0 +1,9 @@
+// Create element and set innerText or innerHTML
+const newChile = document.createElement('li');
+newChile.innerHTML = 'New born baby footballer';
+
+// Find the parent where you will and the child
+const playerList = document.getElementById('player-list');
+
+// Append the child to the parent
+playerList.appendChild(newChile);                                        /* */
